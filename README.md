2a2d5e1c3f120be48ada1273af0ac3a85d55a0cd
HSnils <hrsnils@gmail.com>
2018-02-08 21:26:10 +0100

    Added items to the spinner

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # IMT3673
-Lab 1-4 in the course IMT3673
+Lab 1-4 in the course **IMT3673**
 
-# Resourses used:
-https://developer.android.com/training/basics/firstapp/starting-activity.html#CreateActivity
+##Resourses used for lab 1:
+* Intent https://developer.android.com/training/basics/firstapp/starting-activity.html#CreateActivity
+* ActivityForResult https://developer.android.com/training/basics/intents/result.html
+* Spinner/Dropdown: https://developer.android.com/guide/topics/ui/controls/spinner.html
 
-https://developer.android.com/training/basics/intents/result.html
+##Resourses used for lab 2:
+
+##Resourses used for lab 3:
+
+##Resourses used for lab 4:
--- lab1/app/src/main/java/com/ntnu/henrik/lab1/A1.java
+++ lab1/app/src/main/java/com/ntnu/henrik/lab1/A1.java
@@ -1,10 +1,14 @@
 package com.ntnu.henrik.lab1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class A1 extends AppCompatActivity {
 
@@ -15,6 +19,34 @@ public class A1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a1);
+
+        //finds the dropdown-menu
+        Spinner spinner = (Spinner) findViewById(R.id.L1);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.L1_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+
+    //OnSelected listener for dropdown
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
+        }
     }
 
     //onclick function on button A1 to go to A2
