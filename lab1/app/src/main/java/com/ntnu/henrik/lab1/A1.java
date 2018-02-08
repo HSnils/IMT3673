d5fa17f4a866ab4946ecdf89d6d0e97531b0abae
HSnils <hrsnils@gmail.com>
2018-02-08 16:03:07 +0100

    Uses INTENT to send message from A1 to A2

--- lab1/app/src/main/java/com/ntnu/henrik/lab1/A1.java
+++ lab1/app/src/main/java/com/ntnu/henrik/lab1/A1.java
@@ -1,11 +1,14 @@
 package com.ntnu.henrik.lab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class A1 extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.ntnu.henrik.lab1.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +16,11 @@ public class A1 extends AppCompatActivity {
         setContentView(R.layout.activity_a1);
     }
 
-    public void onButtonTap(View v){
-        Toast myToast = Toast.makeText(
-                getApplicationContext(),
-                "TEST!",
-                Toast.LENGTH_LONG);
-        myToast.show();
+    public void onClickButtonA1(View view){
+        Intent intent = new Intent(this, A2.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
