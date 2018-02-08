085c606146d4d1be65dbf24fa535f92e2cc84649
HSnils <hrsnils@gmail.com>
2018-02-08 15:33:36 +0100

    Added A1-A3 pages with elements, and a toast testfunction on button

--- lab1/app/src/main/java/com/ntnu/henrik/lab1/A1.java
+++ lab1/app/src/main/java/com/ntnu/henrik/lab1/A1.java
@@ -2,6 +2,8 @@ package com.ntnu.henrik.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class A1 extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class A1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a1);
+    }
+
+    public void onButtonTap(View v){
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "TEST!",
+                Toast.LENGTH_LONG);
+        myToast.show();
     }
 }
